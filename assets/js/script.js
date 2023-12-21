c058a9e5086fb3478abddfdd25896526bbd7799c
Alexis Noriega <alexisricardonoriegamoreno@gmail.com>
2023-12-21 17:59:25 -0600

    se agrego funcionalidad del boton

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
 const btn = document.getElementById('btn')
+const colors = ['bluesky','ligthgreen', 'brown', 'black', 'darkblue', 'orange', 'yellowgreen', 'purple']
+function randomColor(length) {
+    
+    return Math.floor(Math.random() * length)
+}
 
 btn.onclick = function(){
-    alert('hello world')
+    let colorDeFondo = colors[randomColor(colors.length)]
+    let colorDeLetra;
+    do {
+        colorDeLetra = colors[randomColor(colors.length)]
+    } while (colorDeFondo === colorDeLetra)
+
+
+
+
+    document.body.style.backgroundColor = colorDeFondo
+    document.getElementById('titulo').style.color = colorDeLetra
 }
